fd4fc7bf83706d51fa2b29002fa3b5237a6ef15b
Ruiqi Tian <rtian25@uwo.ca>
2022-03-14 20:03:05 -0400

    initial commit

---
+++ D3QNAgent.py
@@ -0,0 +1,337 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+import numpy as np
+import random
+
+
+class D3QN(keras.Model):
+    
+    
+    def __init__(self, n_actions, d1_dims, d2_dims):
+        '''D3QN class constructor
+        
+        Args:
+            n_actions (int): number of possible actions
+            d1_dims (int): number of neurons in the first dense layer
+            d2_dims (int): number of neurons in the second dense layer
+        
+        Returns:
+            None
+        
+        '''
+        
+        # super constructor
+        super(D3QN, self).__init__()
+        
+        # define dueling double deep Q network (D3QN)
+        # because observation is a matrix of size 6 x 7, there is really
+        # no need for convolution layers, hence two dense layers are used 
+        # here
+        self.dense1 = keras.layers.Dense(d1_dims, activation='relu')
+        self.dense2 = keras.layers.Dense(d2_dims, activation='relu')
+        
+        # Value output
+        self.V = keras.layers.Dense(1, activation=None)
+        
+        # Advantage output
+        self.A = keras.layers.Dense(n_actions, activation=None)
+        
+        pass
+    
+    def call(self, state):
+        '''override call function
+        
+        Args:
+            state: the current state of the environment
+            
+        Returns:
+            Q
+        '''
+        
+        x = self.dense1(state)
+        x = self.dense2(x)
+        V = self.V(x)
+        A = self.A(x)
+        
+        # Q is calculated as defined in the paper "Dueling Network 
+        # Architectures for Deep Reinforcement Learning"
+        Q = (V + A - tf.math.reduce_mean(A, axis=1, keepdims=True))
+        
+        return Q
+    
+    def advantage(self, state):
+        '''calculates and returns Advantage
+        
+        Args:
+            state: the current state of the environment
+            
+        Returns:
+            A: Advantage
+        
+        '''
+        
+        x = self.dense1(state)
+        x = self.dense2(x)
+        A = self.A(x)
+        
+        return A
+    
+    pass
+
+class ReplayBuffer():
+    
+    def __init__(self, size):
+        '''replay buffer constructor
+        
+        Args:
+            size (int): maximum size of replay buffer
+            
+        Returns:
+            None
+        '''
+        
+        # deque overrides the oldest entry with the newest entry, which
+        # makes it ideal for buffer storage
+        self.replay_buffer = deque(maxlen=size)
+        
+        pass
+    
+    def update_buffer(self, transition):
+        '''update replay buffer with new transition
+        
+        Args:
+            transition (tuple): \
+                (current_state, action, reward, new_state, done)
+        
+        Returns:
+            None
+        '''
+        self.replay_buffer.append(transition)
+        
+        pass
+    
+    def size(self):
+        '''returns the length of replay buffer
+        
+        Args:
+            None
+            
+        Returns:
+            length of replay buffer
+        '''
+        
+        return len(self.replay_buffer)
+    
+    pass
+
+    def random_sample(self, size):
+        '''returns a random sample of replay buffer with given size
+        
+        Args:
+            size: size of random sample
+        
+        Returns:
+            random sample of replay buffer
+        
+        '''
+        return random.sample(self.replay_buffer, size)
+
+
+class Agent():
+    
+    def __init__(self, config, lr, gamma, batch_size, epsilon, 
+                 eps_dec=1e-3, eps_min=1e-2, buff_size=1_000_000, 
+                 d1_dims=128, d2_dims=128, replace_target_weight=10):
+        '''agent constructor
+        
+        Args:
+            config: environment configuration
+            lr: learning rate
+            gamma: discount
+            epsilon: epsilon that controls epsilon greedy policy
+            eps_dec: epsilon decay
+            eps_min: minimum epsilon
+            batch_size: batch size for minibatch traning
+            buff_size: size of replay buffer
+            d1_dims: dimension of dense1 layer
+            d2_dims: dimension of dense2 layer
+            replace_target_weight: number of episodes before target 
+                                   weights are updated
+            
+        Returns:
+            None
+        '''
+        
+        # extract number of actions from environment configuration
+        self.n_actions = config.columns
+        
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_dec = eps_dec
+        self.eps_min = eps_min
+        self.batch_size = batch_size
+        
+        # define model and target
+        self.model = D3QN(self.n_actions, d1_dims, d2_dims)
+        self.target = D3QN(self.n_actions, d1_dims, d2_dims)
+        
+        # compile models
+        self.model.compile(loss='mse', optimizer=Adam(learning_rate=lr))
+        # won't actually optimize
+        self.target.compile(loss='mse', optimizer=Adam(learning_rate=lr)) 
+        
+        # copy model weights to target weights
+        self.target.set_weights(self.model.get_weights())        
+        
+        # define replay buffer
+        self.replay_buffer = ReplayBuffer(buff_size)
+        
+        # define replace_target_weight_threshold and counter
+        self.target_replace_counter = 0
+        self.replace_target_weight = replace_target_weight
+        
+        pass
+    
+    def update_replay_buffer(self, transition):
+        '''method to update replay buffer with new transition
+        
+        Args:
+            transition: new transition of type tuple \
+                (current_state, action, reward, new_state, done)
+                
+        Returns:
+            None
+        '''
+        
+        self.replay_buffer.update_buffer(transition)
+        
+        pass
+    
+    def save_model(self, path):
+        '''save the target model weights to a file
+        
+        Args:
+            path: filepath
+        
+        Returns:
+            None
+        '''
+    
+        self.target.save(path)
+        
+        
+    def load_model(self, path):
+        '''load the model weights from a file
+        
+        Args:
+            path: filepath
+            
+        Returns:
+            None
+        
+        '''
+        
+        self.model.load_model(path)
+    
+    def choose_action(self, observation):
+        '''method to choose an action with a given observation with
+        epsilon greedy policy
+        
+        Args:
+            observation: the current observation from the environment
+            
+        Returns:
+            action: the action chosen
+        
+        '''
+        
+        
+        
+        mask = [True if observation[idx] == 0 else False \
+                for idx in range(self.n_actions)]
+        
+        if np.random.random() < self.epsilon:
+            # pick random action from valid action space
+            
+            valid_action_space = np.where(mask)[0]
+            action = np.random.choice(valid_action_space)
+            
+        else:
+            state = np.array([observation])
+            advantages = self.model.advantage(state).numpy().flatten()
+            # mask off invalid actions
+            
+            valid_advantages = [advantages[idx] 
+                                if mask[idx] else np.NaN
+                               for idx in range(self.n_actions)]
+            
+            action = np.nanargmax(valid_advantages)
+#             print(action, type(action), type(action.item()))
+        return action.item()
+        
+        
+    def learn(self, terminated):
+        # if replay buffer has not been fully populated, do not learn
+        if self.replay_buffer.size() < self.batch_size:
+            return
+        
+        # sample a minibatch of transitions from replay buffer
+        batch = self.replay_buffer.random_sample(self.batch_size)
+        
+        # extract current states and predict current Q's
+        curr_states_list = np.array([transition[0] for transition in batch])
+        curr_Qs_list = self.model.predict(curr_states_list)
+        
+        # extract next states and predict next Q's
+        next_states_list = np.array([transition[3] for transition in batch])
+        next_Qs_list = self.target.predict(next_states_list)
+        
+        X = [] # training X, states
+        y = [] # training y, Q values
+        
+        # for each transition in batch, if it is not a terminal state,
+        # then get the max Q value from the future Q values and follow 
+        # formula to calculate new Q value
+        # if the state is a terminal state, set the new Q value to reward
+        for idx, (state, action, reward, state_, done) in enumerate(batch):
+            if not done:
+                max_next_Q = np.max(next_Qs_list[idx])
+                new_Q = reward + self.gamma * max_next_Q
+                
+            else:
+                new_Q = reward
+            
+            # update the Q value corresponding to the action
+            curr_Qs = curr_Qs_list[idx]
+            curr_Qs[action] = new_Q
+            
+            # append this traning sample for DQN model refit
+            X.append(state)
+            y.append(curr_Qs)
+            
+            pass
+        
+        # refit the model with minibatch
+        self.model.fit(np.array(X), np.array(y), 
+                       batch_size=self.batch_size, verbose=0, 
+                       shuffle=False)
+        
+        
+        
+        
+    def evolve(self):
+        # update target replace counter at the end of each episode
+        self.target_replace_counter += 1
+
+        # replace target weights with model weights 
+        if self.target_replace_counter > self.replace_target_weight:
+            self.target.set_weights(self.model.get_weights())
+            self.target_replace_counter = 0
+            # print('Target model weights updated.')
+
+        # Decay epsilon
+        if self.epsilon > self.eps_min:
+            self.epsilon *= self.eps_dec
+            self.epsilon = max(self.epsilon, self.eps_min)
---
+++ fast_Nstep_lookahead_agent.py
@@ -0,0 +1,180 @@
+# Importing Dependencies
+import random  # connect the library for working with random numbers
+import numpy as np  # connect the library general mathematical and numerical operations
+'''
+Helper Functions:
+- score_move_a: calculates score if agent drops piece in selected column
+- score_move_b: calculates score if opponent drops piece in selected column
+- drop_piece: return grid status after player drops a piece
+- get_heuristic: calculates value of heuristic for grid
+- get_heuristic_optimised: calculates value of heuristic optimised
+- check_window: checks if window satisfies heuristic conditions
+- count_windows: counts number of windows satisfying specified heuristic conditions
+- count_windows_optimised: counts number of windows satisfying specified heuristic optimised conditions
+'''
+
+# Calculates score if agent drops piece in selected column
+def score_move_a(grid, col, mark, config, start_score, n_steps):
+    next_grid, pos = drop_piece(grid, col, mark, config)
+    row, col = pos
+    score = get_heuristic_optimised(grid,next_grid,mark,config, row, col,start_score)
+    valid_moves = [col for col in range (config.columns) if next_grid[0][col]==0]
+    '''Since we have just dropped our piece there is only the possibility of us getting 4 in a row and not the opponent.
+    Thus score can only be +infinity'''
+    scores = []
+    if len(valid_moves)==0 or n_steps ==0 or score == float("inf"):
+        return score
+    else :
+        for col in valid_moves:
+            current = score_move_b(next_grid,col,mark,config,score,n_steps-1)
+            scores.append(current)
+        score = min(scores)
+    return score
+
+# calculates score if opponent drops piece in selected column
+def score_move_b(grid, col, mark, config, start_score, n_steps):
+    next_grid, pos = drop_piece(grid,col,(mark%2)+1,config)
+    row, col = pos
+    score = get_heuristic_optimised(grid,next_grid,mark,config, row, col,start_score)
+    valid_moves = [col for col in range (config.columns) if next_grid[0][col]==0]
+    '''
+    Since we have just dropped opponent piece there is only the possibility of opponent getting 4 in a row and not us.
+    Thus score can only be -infinity.
+    '''
+    scores = []
+    if len(valid_moves)==0 or n_steps ==0 or score == float ("-inf"):
+        return score
+    else :
+        for col in valid_moves:
+            current = score_move_a (next_grid,col,mark,config,score,n_steps-1)
+            scores.append(current)
+        score = max(scores)
+    return score
+
+# Gets board at next step if agent drops piece in selected column
+def drop_piece(grid, col, mark, config):  # 
+    next_grid = grid.copy()  # make a copy of the location of the chips on the playing field for its further transformation
+    for row in range(config.rows-1, -1, -1):  # iterate over all rows in the playing field
+        if next_grid[row][col] == 0:  # we are not interested in empty cells
+            break  # we skip them if we meet such
+    next_grid[row][col] = mark # mark the cell in which our chip will fall
+    return next_grid,(row,col) # return board at next step
+
+# calculates value of heuristic for grid
+def get_heuristic(grid, mark, config):
+    score = 0
+    num = count_windows(grid,mark,config)
+    for i in range(config.inarow):
+        #num  = count_windows (grid,i+1,mark,config)
+        if (i==(config.inarow-1) and num[i+1] >= 1):
+            return float("inf")
+        score += (4**(i))*num[i+1]
+    num_opp = count_windows (grid,mark%2+1,config)
+    for i in range(config.inarow):
+        if (i==(config.inarow-1) and num_opp[i+1] >= 1):
+            return float ("-inf")
+        score-= (2**((2*i)+1))*num_opp[i+1]
+    return score
+
+# calculates value of heuristic optimised
+def get_heuristic_optimised(grid, next_grid, mark, config, row, col, start_score):
+    score = 0
+    num1 = count_windows_optimised(grid,mark,config,row,col)
+    num2 = count_windows_optimised(next_grid,mark,config,row,col)
+    for i in range(config.inarow):
+        if (i==(config.inarow-1) and (num2[i+1]-num1[i+1]) >= 1):
+            return float("inf")
+        score += (4**(i))*(num2[i+1]-num1[i+1])
+    num1_opp = count_windows_optimised(grid,mark%2+1,config,row,col)
+    num2_opp = count_windows_optimised(next_grid,mark%2+1,config,row,col)
+    for i in range(config.inarow): 
+        if (i==(config.inarow-1) and num2_opp[i+1]-num1_opp[i+1]  >= 1):
+            return float ("-inf")     
+        score-= (2**((2*i)+1))*(num2_opp[i+1]-num1_opp[i+1])
+    score+= start_score
+    return score
+
+# checks if window satisfies heuristic conditions
+def check_window(window, piece, config):
+    if window.count((piece%2)+1)==0:
+        return window.count(piece)
+    else:
+        return -1
+
+# counts number of windows satisfying specified heuristic conditions
+def count_windows(grid, piece, config):
+    num_windows = np.zeros(config.inarow+1)
+    # horizontal
+    for row in range(config.rows):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[row, col:col+config.inarow])
+            type_window = check_window(window, piece, config)
+            if type_window != -1:
+                num_windows[type_window] += 1
+    # vertical
+    for row in range(config.rows-(config.inarow-1)):
+        for col in range(config.columns):
+            window = list(grid[row:row+config.inarow, col])
+            type_window = check_window(window, piece, config)
+            if type_window != -1:
+                num_windows[type_window] += 1
+    # positive diagonal
+    for row in range(config.rows-(config.inarow-1)):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+            type_window = check_window(window, piece, config)
+            if type_window != -1:
+                num_windows[type_window] += 1
+    # negative diagonal
+    for row in range(config.inarow-1, config.rows):
+        for col in range(config.columns-(config.inarow-1)):
+            window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+            type_window = check_window(window, piece, config)
+            if type_window != -1:
+                num_windows[type_window] += 1
+    return num_windows
+
+# counts number of windows satisfying specified heuristic optimised conditions
+def count_windows_optimised(grid, piece, config, row, col):
+    num_windows = np.zeros(config.inarow+1)
+    # horizontal
+    for acol in range(max(0,col-(config.inarow-1)),min(col+1,(config.columns-(config.inarow-1)))):
+        window = list(grid[row, acol:acol+config.inarow])
+        type_window = check_window(window, piece, config)
+        if type_window != -1:
+            num_windows[type_window] += 1
+    # vertical
+    for arow in range(max(0,row-(config.inarow-1)),min(row+1,(config.rows-(config.inarow-1)))):
+        window = list(grid[arow:arow+config.inarow, col])
+        type_window = check_window(window, piece, config)
+        if type_window != -1:
+            num_windows[type_window] += 1
+    # positive diagonal
+    for arow, acol in zip(range(row-(config.inarow-1),row+1),range(col-(config.inarow-1),col+1)):
+        if (arow>=0 and acol>=0 and arow<=(config.rows-config.inarow) and acol<=(config.columns-config.inarow)):
+            window = list(grid[range(arow, arow+config.inarow), range(acol, acol+config.inarow)])
+            type_window = check_window(window, piece, config)
+            if type_window != -1:
+                num_windows[type_window] += 1
+    # negative diagonal
+    for arow,acol in zip(range(row,row+config.inarow),range(col,col-config.inarow,-1)):
+        if (arow >= (config.inarow-1) and acol >=0 and arow <= (config.rows-1) and acol <= (config.columns-config.inarow)):
+            window = list(grid[range(arow, arow-config.inarow, -1), range(acol, acol+config.inarow)])
+            type_window = check_window(window, piece, config)
+            if type_window != -1:
+                num_windows[type_window] += 1
+    return num_windows
+
+# How deep to make the game tree: higher values take longer to run!
+N_STEPS = 3
+# main function of our agent
+def agent(obs, config):
+    grid = np.asarray(obs.board).reshape(config.rows, config.columns)
+    valid_moves = [c for c in range(config.columns) if grid[0][c] == 0]
+    scores = {}
+    start_score = get_heuristic(grid, obs.mark, config)
+    for col in valid_moves:
+        scores[col] = score_move_a(grid, col, obs.mark, config,start_score, N_STEPS)
+#     print (N_STEPS, "-step lookahead agent:",scores)
+    max_cols = [key for key in scores.keys() if scores[key] == max(scores.values())]
+    return random.choice(max_cols)
